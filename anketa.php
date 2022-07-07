f3de1bdd114e1b550562445fe3f6cbb9fe93b719
ariavio <98454303+ariavio@users.noreply.github.com>
2022-07-07 10:35:29 +0300

    Create anketa.php

---
+++ anketa.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+  <title>Анкета</title>
+  <link rel="shortcut icon" href="icons8-звезда-64.png" type="image/x-icon">
+
+</head>
+<body>
+
+        <?php
+        echo("hello!");
+        ?>
+
+     </body>
+</html>
